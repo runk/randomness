a1cde2632c55b73b0f38ff6e441992c9741c544b
Dmitry Shirokov <dshirokov@seek.com.au>
2022-11-03 16:22:46 +1100

    chore: remove jest config

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testRegex: '.*spec.ts$',
-  transform: { '^.+\\.ts?$': 'ts-jest' },
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  rootDir: 'src',
-};
